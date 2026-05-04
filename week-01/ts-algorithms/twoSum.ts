fd1eec394718c10a4cd779de0af161ad5e89e5a3
Leo <leonardhawkesjr@gmail.com>
2026-05-04 00:04:37 -0400

    revert: leave problems as stubs for self-solving

--- week-01/ts-algorithms/twoSum.ts
+++ week-01/ts-algorithms/twoSum.ts
@@ -9,15 +9,6 @@
  * Space: O(n)
  */
 export function twoSum(nums: number[], target: number): [number, number] {
-  const seen = new Map<number, number>(); // value → index
-
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (seen.has(complement)) {
-      return [seen.get(complement)!, i];
-    }
-    seen.set(nums[i], i);
-  }
-
-  throw new Error("No solution found");
+  // TODO: implement
+  throw new Error("Not implemented");
 }
